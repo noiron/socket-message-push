3f9cd3de7f69f5bd20cf294b501f280301035aaa
wu kai <wukai2048@gmail.com>
2017-11-07 17:37:26 +0800

    加入重连次数限制

--- client/client.js
+++ client/client.js
@@ -1,7 +1,9 @@
 // 此处路径应根据服务器进行设置
 const listenUrl = 'http://localhost:4001';
 
-const msgSocket = io(listenUrl);
+const msgSocket = io(listenUrl, {
+    'reconnectionAttempts': 10,     // 限制对于 socket 服务器的重连次数为10次
+});
 
 const name = prompt("Please enter your name", "Jon Snow");
 const tokenId = prompt("Please enter your tokenId", "1");
